1a900f13eb22fc35f0378e1585c1ba8d60ca2d7f
kaizoku-oh <garssallaoui.bayrem@gmali.com>
2023-10-22 17:40:28 +0100

    Adding die temperature code

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
+#ifdef DIE_TEMPERATURE
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#endif // DIE_TEMPERATURE
 
 #include "Button.h"
 #include "Led.h"
 
 #define MAIN_THREAD_SLEEP_TIME_MS (100)
+
+#ifdef DIE_TEMPERATURE
+static void test_die_temperature();
+#endif // DIE_TEMPERATURE
 
 int main(void) {
   const struct gpio_dt_spec buttonGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
@@ -33,3 +41,34 @@ int main(void) {
 
   return EXIT_SUCCESS;
 }
+
+#ifdef DIE_TEMPERATURE
+static void test_die_temperature() {
+  const struct device *const sensorDevice = DEVICE_DT_GET(DT_NODELABEL(die_temp));
+
+  int ret = 0;
+  struct sensor_value value = {0};
+
+  if (device_is_ready(sensorDevice)) {
+    return;
+  }
+
+  while (true) {
+    ret = sensor_sample_fetch(sensorDevice);
+    if (ret) {
+      printk("Failed to fetch sample (%d)\n", ret);
+      return;
+    }
+
+    ret = sensor_channel_get(sensorDevice, SENSOR_CHAN_DIE_TEMP, &value);
+    if (ret) {
+      printk("Failed to get data (%d)\n", ret);
+      return;
+    }
+
+    printk("CPU Die temperature: %.1f °C\r\n", sensor_value_to_double(&value));
+
+    k_msleep(MAIN_THREAD_SLEEP_TIME_MS);
+  }
+}
+#endif // DIE_TEMPERATURE
