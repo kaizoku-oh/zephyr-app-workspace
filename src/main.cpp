1e61a262388f3b5c29250bcd87829af539130a1a
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2023-12-06 23:37:40 +0100

    Add multi-threading (#6)
    * Add multi-threading code
    
    * Update footprint table

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include <zephyr/device.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/ring_buffer.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_core.h>
 #include <zephyr/net/net_context.h>
@@ -19,46 +18,83 @@
 #include "Serial.h"
 #include "Network.h"
 
-#define MAIN_THREAD_SLEEP_TIME_MS (100)
-#define RING_BUFFER_SIZE (1024)
+#define MAIN_THREAD_SLEEP_TIME_MS        (1000)
+#define LED_THREAD_SLEEP_TIME_MS         (500)
+#define TEMPERATURE_THREAD_SLEEP_TIME_MS (1000)
+#define BUTTON_THREAD_SLEEP_TIME_MS      (100)
+#define NETWORK_THREAD_SLEEP_TIME_MS     (1000)
 
-static struct ring_buf ringBuffer;
-static uint8_t buffer[RING_BUFFER_SIZE];
+static void led_thread_handler(void);
+static void temperature_thread_handler(void);
+static void button_thread_handler(void);
+static void network_thread_handler(void);
+
+K_THREAD_DEFINE(led_thread, 512, led_thread_handler, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(temperature_thread, 512, temperature_thread_handler, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(button_thread, 512, button_thread_handler, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(network_thread, 512, network_thread_handler, NULL, NULL, NULL, 7, 0, 0);
 
 int main(void) {
-  // Local variables used to read data from ring buffer
-  uint8_t rxData[8] = {0};
-  uint8_t length = 0;
+  while (true) {
+    k_msleep(MAIN_THREAD_SLEEP_TIME_MS);
+  }
 
+  return EXIT_SUCCESS;
+}
+
+static void led_thread_handler(void) {
   // Reference devices from device tree
-  const struct gpio_dt_spec buttonGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
   const struct gpio_dt_spec greenLedGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
   const struct gpio_dt_spec blueLedGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {0});
   const struct gpio_dt_spec redLedGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios, {0});
-  const struct device *temperatureDevice = DEVICE_DT_GET(DT_NODELABEL(die_temp));
-  const struct device *serialDevice = DEVICE_DT_GET(DT_NODELABEL(usart2));
 
-  // Get the singleton instance of Network
-  Network& network = Network::getInstance();
-
-  // Create local objects using the device tree devices
-  Button button(&buttonGpio);
+  // Create local object using the device tree device
   Led greenLed(&greenLedGpio);
   Led blueLed(&blueLedGpio);
   Led redLed(&redLedGpio);
+
+  // Continuously toggle LEDs
+  while (true) {
+    greenLed.toggle();
+    blueLed.toggle();
+    redLed.toggle();
+    k_msleep(LED_THREAD_SLEEP_TIME_MS);
+  }
+}
+
+static void temperature_thread_handler(void) {
+  // Reference device from device tree
+  const struct device *temperatureDevice = DEVICE_DT_GET(DT_NODELABEL(die_temp));
+
+  // Create local object using the device tree device
   Temperature temperature(temperatureDevice);
-  Serial serial(serialDevice);
 
-  // Link buffer buffer to ringBuffer and initialize them
-  ring_buf_init(&ringBuffer, sizeof(buffer), buffer);
+  // Continuously read temperature
+  while (true) {
+    printk("CPU temperature: %.1f °C\r\n", temperature.read());
+    k_msleep(TEMPERATURE_THREAD_SLEEP_TIME_MS);
+  }
+}
 
-  // Register serial callback as a lambda function
-  serial.onReceive([](uint8_t *data, uint32_t length) {
-    ring_buf_put(&ringBuffer, data, length);
-  });
+static void button_thread_handler(void) {
+  // Reference device from device tree
+  const struct gpio_dt_spec buttonGpio = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
 
-  // Write a string over serial
-  serial.write((uint8_t *)"Hello world!\r\n", (sizeof("Hello world!\r\n") - 1));
+  // Create local object using the device tree device
+  Button button(&buttonGpio);
+
+  // Continuously check if a button is pressed
+  while (true) {
+    if (button.isPressed()) {
+      printk("Button is pressed\r\n");
+    }
+    k_msleep(BUTTON_THREAD_SLEEP_TIME_MS);
+  }
+}
+
+static void network_thread_handler(void) {
+  // Get the singleton instance of Network
+  Network& network = Network::getInstance();
 
   // Set up the lambda callback for IP address notification
   network.onGotIP([](const char *ipAddress) {
@@ -68,19 +104,7 @@ int main(void) {
   // Start the network and wait for an IP address
   network.start();
 
-  // Continuously check if a button is pressed, if so read temperature and toggle LEDs
   while (true) {
-    if (button.isPressed()) {
-      printk("Button is pressed\r\n");
-      printk("CPU temperature: %.1f °C\r\n", temperature.read());
-      length = ring_buf_get(&ringBuffer, rxData, sizeof(rxData));
-      printk("Received data: %.*s\r\n", length, rxData);
-      redLed.toggle();
-      greenLed.toggle();
-      blueLed.toggle();
-    }
-    k_msleep(MAIN_THREAD_SLEEP_TIME_MS);
+    k_msleep(NETWORK_THREAD_SLEEP_TIME_MS);
   }
-
-  return EXIT_SUCCESS;
 }
